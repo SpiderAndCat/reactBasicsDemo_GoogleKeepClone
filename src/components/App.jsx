b6aaf4cedef6202b01fef63a3aee1f5269f735c5
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2024-02-24 19:24:39 -0700

    Hardcoded note examples added in `notes.js` to map into App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes.js";
 
 function App() {
   return (
